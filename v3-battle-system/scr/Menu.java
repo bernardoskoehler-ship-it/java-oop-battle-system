1242cf833648651805639279b1d52c03e29550d0
Koehler <bernardoskoehler@gmail.com>
2026-05-05 23:51:36 -0300

    Update Menu.java

--- v3-battle-system/scr/Menu.java
+++ v3-battle-system/scr/Menu.java
@@ -1,1 +1,9 @@
+import java.util.Scanner;
 
+public class Menu {
+    Scanner scanner = new Scanner(System.in);
+
+     public void iniciarJogo() {
+         System.out.println("Bem vindo a 3 versao do meu Sistema de Batalhas RPJ.");
+     }
+}
