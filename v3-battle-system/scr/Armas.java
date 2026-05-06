fe671d30199a2b9bd46aa241ad9e913dbbaa0451
Koehler <bernardoskoehler@gmail.com>
2026-05-06 16:06:29 -0300

    Implement Armas class with weapon attributes
    Added constructor and methods for Armas class to handle weapon attributes.

--- v3-battle-system/scr/Armas.java
+++ v3-battle-system/scr/Armas.java
@@ -1,3 +1,32 @@
 public class Armas {
-    
+
+    private int dano;
+    private int gastoEstamina;
+
+    public Armas(String nome) {
+        nome = nome.toLowerCase();
+
+        if (nome.equals("adaga")) {
+            dano = 5;
+            gastoEstamina = 2;
+
+        } else if (nome.equals("espada")) {
+            dano = 10;
+            gastoEstamina = 5;
+
+        } else if (nome.equals("machado")) {
+            dano = 20;
+            gastoEstamina = 10;
+
+        } else {
+            throw new IllegalArgumentException("Arma inválida: " + nome);
+        }
+    }
+
+    public int getDano() {
+        return dano;
+    }
+    public int getGastoEstamina() {
+        return gastoEstamina;
+    }
 }
