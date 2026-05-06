a9c5308e55c3fe400f775de96075dc0c82c5511e
Koehler <bernardoskoehler@gmail.com>
2026-05-06 16:06:45 -0300

    Implement Armaduras class with durability and stamina

--- v3-battle-system/scr/Armaduras.java
+++ v3-battle-system/scr/Armaduras.java
@@ -1,3 +1,29 @@
 public class Armaduras {
-    
+
+    private int durabilidade;
+    private int gastoEstamina;
+
+    public Armadura(String nome) {
+        nome = nome.toLowerCase();
+
+        if (nome.equals("leve")) {
+            durabilidade = 10;
+            gastoEstamina = 0;
+
+        } else if (nome.equals("media")) {
+            durabilidade = 20;
+            gastoEstamina = 5;
+
+        } else if (nome.equals("pesada")) {
+            durabilidade = 30;
+            gastoEstamina = 10;
+
+        } else {
+            throw new IllegalArgumentException("Armadura inválida: " + nome);
+        }
+    }
+
+    public int getDurabilidade() {
+        return durabilidade;
+    }
 }
