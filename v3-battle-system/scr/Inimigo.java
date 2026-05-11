143ce04819809e91a70b53d2d2a99eb3fe245810
Koehler <bernardoskoehler@gmail.com>
2026-05-11 13:02:01 -0300

    Update Inimigo.java

--- v3-battle-system/scr/Inimigo.java
+++ v3-battle-system/scr/Inimigo.java
@@ -1,6 +1,22 @@
+import java.util.Random;
+
 public class Inimigo extends Personagem{
+    Random random = new Random();
 
     Inimigo(String nome) {
         super(nome);
+        escolherEquipamento();
+    }
+
+    public String escolherRandom(String... nomes) {
+
+        int numero = random.nextInt(nomes.length);
+
+        return nomes[numero];
+    }
+
+    public void escolherEquipamento() {
+        escolherArma(escolherRandom("adaga", "espada", "machado"));
+        escolherArmadura(escolherRandom("leve", "media", "pesada"));
     }
 }
