7d76c5f03299cd3d5da2ea306dd30a3d7a253ee1
Koehler <bernardoskoehler@gmail.com>
2026-05-11 16:41:55 -0300

    Add action handling method to Jogador class

--- v3-battle-system/scr/Jogador.java
+++ v3-battle-system/scr/Jogador.java
@@ -3,4 +3,24 @@ public class Jogador extends Personagem {
     Jogador(String nome) {
         super(nome);
     }
+
+    public boolean agir(String acao, Personagem alvo){
+        acao = acao.toLowerCase();
+        switch(acao) {
+            case "ataque fraco":
+                ataqueFraco(alvo);
+                return true;
+            case "ataque forte":
+                ataqueForte(alvo);
+                return true;
+            case "descansar":
+                descansar();
+                return true;
+            case "status":
+                mostrarStatus();
+                return true;
+            default:
+                return false;
+        }
+    }
 }
