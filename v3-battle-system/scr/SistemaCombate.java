77b77c1c9b6ec272061e39aa1225c5c2babe600a
Koehler <bernardoskoehler@gmail.com>
2026-05-11 22:04:46 -0300

    Enhance combat system with player actions and enemy turns

--- v3-battle-system/scr/SistemaCombate.java
+++ v3-battle-system/scr/SistemaCombate.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class SistemaCombate {
+    private Scanner scanner = new Scanner(System.in);
+
     public void iniciarCombate(Jogador jogador, Inimigo inimigo) {
         while(jogador.estaVivo() && inimigo.estaVivo()) {
-            
+            System.out.println("Ataque Fraco: " +jogador.getDanoArma());
+            System.out.println("Ataque Forte: " +(jogador.getDanoArma()+5));
+            System.out.println("Descansar");
+            System.out.println("Status");
+
+            String acao = scanner.nextLine();
+            if(!jogador.agir(acao, inimigo)) {
+                continue;
+            }
+            System.out.println("\n=== NOVO TURNO ===");
+            if(!inimigo.estaVivo()) {
+                break;
+            }
+            inimigo.agir(jogador);
+            System.out.println("\n=== NOVO TURNO ===");
         }
+        System.out.println("Fim do duelo!");
     }
+
 }
